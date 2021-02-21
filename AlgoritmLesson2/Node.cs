55977fb1d001bb7a4bc7f95497f34569ff7d0e14
Evgeny <chempion_93_93@mail.ru>
2021-02-21 17:24:13 +0600

    Добавьте файлы проекта.

---
+++ AlgoritmLesson2/Node.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmLesson2
+{
+    public class Node : ILinkedList
+    {
+
+
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PrevNode { get; set; }
+
+        private int count = 0;
+        private Node startNode;
+        private Node endNode;
+
+        public Node() 
+        {
+
+        }
+        public Node(int value) 
+        {
+            Value = value;
+        }
+        public void AddNode(int value)
+        {
+          
+
+            if (count != 0)
+            {
+                
+                endNode.NextNode = new Node(value);
+                endNode.NextNode.PrevNode = endNode;
+                endNode = endNode.NextNode;
+            }
+            else 
+            {
+
+                startNode = new Node(value);
+                startNode.Value = value;
+                endNode = startNode;
+            }
+            count++;
+        }
+
+        public void AddNodeAfter(Node node, int value)
+        {
+            if (CheckNode(node))//Если заданный элемент в списке есть, то работаем
+            {
+                if (node == endNode)//Если элемент последний в списке, то просто добавляем его в конец
+                {
+                    AddNode(value);
+                }
+                else //Если не последний, то вставляем его между элементами
+                {
+                    Node nextNode = node.NextNode;//Сохраняем следующую ноду для дальнейшего использования
+                    node.NextNode = new Node(value);//Создаем новую ноду после найденной
+                    node.NextNode.NextNode = nextNode;//У новой ноды устанавливаем ссылку на следующую
+                    node.NextNode.PrevNode = node;//И на предыдущую
+                    nextNode.PrevNode = node.NextNode;//Обновляем ссылку на предыдущую ноду у ранее сохраненной
+                    count++;//Обновляем количество
+                }
+            }
+        }
+
+        public Node FindNode(int searchValue)
+        {
+            Node currentNode = startNode;
+
+            bool isSearchEnd = false;
+            while (!isSearchEnd)
+            {
+                if (currentNode != null)
+                    if (currentNode.Value == searchValue)
+                        isSearchEnd = true;
+                    else
+                        currentNode = currentNode.NextNode;
+                else
+                    isSearchEnd = true;
+            }
+
+            return currentNode;
+        }
+
+        public int GetCount()
+        {
+            return count;
+        }
+
+        public void RemoveNode(int index)
+        {
+            
+            RemoveNode(FindNodeByIndex(index));
+            
+        }
+
+        public void RemoveNode(Node node)
+        {
+            if (node != null && CheckNode(node))
+            {
+                if (node == startNode) RemoveFirst();
+                else if (node == endNode) RemoveLast();
+                else
+                {
+                    node.PrevNode.NextNode = node.NextNode;
+                    node.NextNode.PrevNode = node.PrevNode;
+                    count--;
+                }
+            }
+        }
+
+        public bool CheckNode(Node node)
+        {
+            bool isNodeInList = false;
+            if (count != 0)
+            {
+                int i = 0;
+                Node currentNode = startNode;
+                while (!isNodeInList && i < count)
+                {
+                    isNodeInList = currentNode == node;
+                    currentNode = currentNode.NextNode;
+                    i++;
+                }
+            }
+            return isNodeInList;
+        }
+
+        public Node FindNodeByIndex(int index)
+        {
+            Node currentNode = null;
+            if (index >= 0 && index < count)
+            {
+                if (index <= count / 2)
+                {
+                    currentNode = startNode;
+                    int i = 0;
+                    while (i < index)
+                    {
+                        currentNode = currentNode.NextNode;
+                        i++;
+                    }
+                }
+                else
+                {
+                    currentNode = endNode;
+                    int i = count - 1;
+                    while (i > index)
+                    {
+                        currentNode = currentNode.PrevNode;
+                        i--;
+                    }
+                }
+            }
+            return currentNode;
+        }
+
+        public void RemoveFirst()
+        {
+            if (count > 1)
+            {
+                startNode.NextNode.PrevNode = null;
+                startNode = startNode.NextNode;
+                count--;
+            }
+            else if (count == 0)
+            {
+                ClearList();
+            }
+        }
+
+        public void RemoveLast()
+        {
+            if (count > 1)
+            {
+                endNode.PrevNode.NextNode = null;
+                endNode = endNode.PrevNode;
+                count--;
+            }
+            else if (count == 0)
+            {
+                ClearList();
+            }
+        }
+
+        public void ClearList()
+        {
+            startNode = null;
+            endNode = null;
+            count = 0;
+        }
+
+    }
+}
+    
+
---
+++ AlgoritmLesson2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoritmLesson2
+{
+    class Program
+    {
+        #region Пример №1
+        static void Main(string[] args)
+        {
+
+            Node Testnode = new Node();
+            Testnode.AddNode(12);
+            Testnode.AddNode(-5);
+            Testnode.AddNode(8);
+            Testnode.AddNode(1231);
+            Testnode.AddNode(11);
+            Testnode.AddNode(-4);
+            Testnode.AddNode(88);
+            Testnode.AddNode(456);
+
+            Console.WriteLine($"Количество элементов в списке: {Testnode.GetCount()}\n");
+            PrintList(Testnode);
+            Console.ReadLine();
+
+           Node newnode = Testnode.FindNodeByIndex(4);
+            Testnode.AddNodeAfter(newnode, 1000);
+            Console.WriteLine("Добавлен элемент со значением 1000, после элемента с индексом 4");
+            PrintList(Testnode);
+            Console.ReadLine();
+
+            Testnode.RemoveFirst();
+            Testnode.RemoveLast();
+            Console.WriteLine("Удалены первый и последний элементы списка");
+            PrintList(Testnode);
+            Console.ReadLine();
+
+            Testnode.RemoveNode(5);
+            Console.WriteLine("Удален элемент с индексом 5");
+            PrintList(Testnode);
+            Console.ReadLine();
+
+            newnode = Testnode.FindNode(88);
+            Testnode.RemoveNode(newnode);
+            Console.WriteLine("Удален элемент со значением 88");
+            PrintList(Testnode);
+            Console.ReadLine();
+
+            Testnode.ClearList();
+            Console.WriteLine("Список полностью очищен");
+            PrintList(Testnode);
+
+            Console.ReadLine();
+
+        }
+
+        private static void PrintList(Node list)
+        {
+            Console.WriteLine("Полный список элементов по индексу:");
+            for (int i = 0; i < list.GetCount(); i++)
+            {
+                Console.WriteLine(i + ":" + list.FindNodeByIndex(i).Value);
+            }
+            Console.WriteLine();
+        }
+        #endregion
+
+        #region Пример № 2
+
+        //Основной цикл проверки, будет выполнятся O(logN) раз, 
+        //все что за его пределами, константные операции и могут быть отброшены.
+        
+
+    /*    public static int BinarySearch(int[] inputArray, int searchValue)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    return mid;
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+
+
+        */
+        #endregion
+    }
+
+}
+
